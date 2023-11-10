6e7fa51cf9ec99ccd6a9dd34990bb3c25b97f5f7
Tolu96 <37343265+Tolu96@users.noreply.github.com>
2023-11-10 14:59:41 +0100

    Deleted Content from App Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'oegs-wpc-frontend';
-}
+export class AppComponent {}
