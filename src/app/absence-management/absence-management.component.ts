ea5aaceb8e7645831653774eccbc57af7e4efe08
Nico Reimann <nreimann107@gmail.com>
2023-11-16 11:52:10 +0100

    sidebar navigation implementation

---
+++ src/app/absence-management/absence-management.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-absence-management',
+  templateUrl: './absence-management.component.html',
+  styleUrls: ['./absence-management.component.scss']
+})
+export class AbsenceManagementComponent {
+
+}
